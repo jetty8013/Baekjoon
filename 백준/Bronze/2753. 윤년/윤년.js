5ffac500d582f724f578e420c01804424e5c8aba
YOONJIWON <112256786+jetty8013@users.noreply.github.com>
2022-09-03 13:10:36 +0900

    [Bronze V] Title: 윤년, Time: 140 ms, Memory: 9352 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.js
@@ -0,0 +1,11 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split(' ');
+
+let a = parseInt(input[0]);
+
+if(a%4 == 0 && a%100 !== 0){
+    console.log(1);
+}else if(a%4 == 0 && a%400 == 0){
+    console.log(1);
+}
+else(console.log(0));
