ab18da5b2aa234f0f150582c2173c0324ae26551
윤지원 <jetty8013@gmail.com>
2022-09-04 15:23:00 +0900

    [Bronze V] Title: A+B - 5, Time: 132 ms, Memory: 9568 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.js
@@ -0,0 +1,8 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+let a;
+
+for(i=0;i<input.length-2;i++){
+    a = input[i].split(' ')
+    console.log(parseInt(a[0])+parseInt(a[1]))
+}
