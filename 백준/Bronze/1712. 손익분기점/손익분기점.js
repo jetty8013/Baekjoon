602c0f350c39a50e307c523ed3537f6934e4139c
윤지원 <jetty8013@gmail.com>
2022-09-06 17:06:36 +0900

    [Bronze II] Title: 손익분기점, Time: 136 ms, Memory: 9592 KB -BaekjoonHub

---
+++ 백준/Bronze/1712. 손익분기점/손익분기점.js
@@ -0,0 +1,11 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split(' ');
+
+const A = input[0] * 1;
+const B = input[1] * 1;
+const C = input[2] * 1;
+
+const margin = C - B;
+const count = Math.floor(A / margin) + 1
+
+console.log(margin <= 0 ? -1 : count);
