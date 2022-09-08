d9b2ec05bb80c1496e4981ef5425a0eca377dc4b
윤지원 <jetty8013@gmail.com>
2022-09-08 18:38:21 +0900

    [Bronze I] Title: 소인수분해, Time: 164 ms, Memory: 9632 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.js
@@ -0,0 +1,21 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+function primeFactors(n){
+  while (n%2 == 0) {
+      console.log(2)
+      n = n/2
+  }
+  // 이제 n은 홀수 -> 이미 2의 배수를 판별했기때문에 2씩 증가시킬 수 있음
+  for (var i=3;i*i <= n;i=i+2) {
+      while (n%i==0) {
+          console.log(i)
+          n = n/i
+      }
+  }
+  if (n>2) {
+      console.log(n)
+  }
+}
+
+primeFactors(input[0])
