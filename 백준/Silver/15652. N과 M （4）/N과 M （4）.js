0860a7bdb9986a81ab33197c0edfa02f7301a2b3
윤지원 <jetty8013@gmail.com>
2022-09-18 20:22:01 +0900

    [Silver III] Title: N과 M (4), Time: 184 ms, Memory: 12128 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.js
@@ -0,0 +1,24 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split(" ").map(Number)
+let visited = new Array(input[0]).fill(0)
+let output = new Array()
+let result = ''
+
+
+function dfs(n,start){
+   if(n === input[1]){
+      result += `${output.join(" ")}\n`
+      return
+   }
+
+   for(let i=start;i<input[0];i++){
+     //if(visited[i]) continue;
+   output.push(i+1)
+   dfs(n+1,i)
+   output.pop()
+   }
+}
+
+dfs(0,0)
+console.log(result.trim())
+
