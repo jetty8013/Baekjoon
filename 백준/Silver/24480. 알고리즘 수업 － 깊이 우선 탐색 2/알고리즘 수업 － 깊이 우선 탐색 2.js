e03fc8c0c58694f079d4b4f4a28ea2bb85014b33
윤지원 <jetty8013@gmail.com>
2022-09-23 00:33:19 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 688 ms, Memory: 100188 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.js
@@ -0,0 +1,31 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+let [n,m,r] = input.shift().split(" ").map(Number)
+let graph = new Array(n+1).fill([]).map(() => new Array())
+let cnt = 1 
+input.forEach(value =>{
+  value = value.split(" ").map(Number)
+  graph[value[0]].push(value[1])
+  graph[value[1]].push(value[0])
+})
+graph.forEach((value,index)=>{
+  graph[index].sort((a,b)=>{return b - a})
+})
+
+let visited = new Array(graph.length).fill(0)
+
+function dfs(num){
+  visited[num] = cnt
+  graph[num].forEach(value=>{
+    if(visited[value] < 1){
+      cnt += 1
+    dfs(value)
+    }
+  })
+  return
+}
+
+dfs(r)
+visited.shift()
+console.log(visited.join("\n"))
