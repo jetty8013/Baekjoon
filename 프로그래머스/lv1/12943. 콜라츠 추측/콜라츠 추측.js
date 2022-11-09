c29b4f72068a95de3b5c8e8e5c96a41ad3edb01c
윤지원 <jetty8013@gmail.com>
2022-11-09 15:29:27 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -0,0 +1,21 @@
+// 콜라츠 독일사람
+// 주어진 조건에 따라 반복문순회
+// 반복문 횟수당 answer +1 
+// 조건성립시 리턴
+
+
+function solution(num) {
+    var answer = 0;
+    
+    while(true){
+    if(num === 1 || num === 0){return answer}
+    else if(answer > 500){return -1}
+    
+    if(num%2 === 0){
+        num = num/2
+    }else if(num%2 === 1){
+        num = (num*3)+1
+    }
+        answer++
+    }
+}
