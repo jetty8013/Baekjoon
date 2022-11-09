043e5cacb36768564145212cd6223e3d5538af7c
윤지원 <jetty8013@gmail.com>
2022-11-09 18:58:23 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.js
+++ 프로그래머스/lv1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.js
@@ -5,7 +5,11 @@
 // join("")으로 합쳐줌
 // number 타입으로 변경후 리턴
 
+
+// sort가 number 타입이 아니여도 정렬 가능
+// .map(Number) 생략 가능
+
 function solution(n) {
-    var answer = n.toString().split('').map(Number).sort((a,b)=>{ return b - a }).join("");
+    var answer = n.toString().split('').sort((a,b)=>{ return b - a }).join("");
     return Number(answer);
 }
