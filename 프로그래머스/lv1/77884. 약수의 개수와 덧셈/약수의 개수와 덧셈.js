6edf0b10c4bdca89c27f93dbad7563711d20ab98
윤지원 <jetty8013@gmail.com>
2022-11-11 20:11:30 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.58 ms, Memory: 35.3 MB -BaekjoonHub

--- 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -1,4 +1,4 @@
-// 1부터 자기 자신까지 0으로 나누어 떨어지는 수의 갯수 찾는 measure 함수 추가로 작성
+// 1부터 자기 자신까지 0으로 나누어 떨어지는 수의 갯수 찾는 divisor 함수 추가로 작성
 
 // left부터 right까지 반복문 범위지정해서 짝수 또는 홀수에 따라 연산
 
@@ -7,14 +7,15 @@ function solution(left, right) {
     var answer = 0;
     
     for(let i = left;i<=right;i++){
-        measure(i)%2 === 0 ? answer+=i : answer-=i
+        divisor(i)%2 === 0 ? answer+=i : answer-=i
     }
+    
     
     return answer;
 }
 
 
-function measure(num){
+function divisor(num){
     let count = 0;
     
     for(let i=1; i<=num;i++){
