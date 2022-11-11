758cf6f07be2fe7bffbb38188b95bf906665e733
윤지원 <jetty8013@gmail.com>
2022-11-11 16:46:57 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
@@ -0,0 +1,40 @@
+// function solution(s) {
+//     var answer = '';
+//     let count = s
+    
+    
+    
+//     count=Math.round(count.length/2)
+    
+//     if(s.length%2 != 0){
+//     s = s.substr(count-1, 1);
+//     }
+//     else if(s.length%2 == 0){
+//         s = s.substr(count-1, 2);
+//     }
+    
+    
+//     return answer = s;
+// }
+// 예전 코드 
+// ,,,, ? 왜저래 .. 
+
+
+
+function solution(s) {
+    let divide_str = 0
+    
+    if(s.length%2 === 0){
+        
+    divide_str = s.length/2
+        
+    return s[divide_str-1]+s[divide_str]
+    }
+    
+    else{
+        
+    divide_str = Math.round(s.length/2)
+    
+    return s[divide_str-1]
+    }
+}
