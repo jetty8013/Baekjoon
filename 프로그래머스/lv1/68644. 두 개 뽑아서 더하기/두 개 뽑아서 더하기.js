59fb3e8773d0f3535b0d548626c8c4269c181b35
윤지원 <jetty8013@gmail.com>
2022-11-18 18:26:05 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.75 ms, Memory: 33.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -0,0 +1,16 @@
+/**
+* 1.반복문으로 모둔 자리 다 더하기
+* 2.filter로 중복 제거하기
+* 3. sort()로 정렬
+*/
+function solution(numbers) {
+  let answer = [];
+  numbers.forEach((element,index) => {
+    for(let i = index+1;i<numbers.length;i++){
+      answer.push(element + numbers[i])
+    }
+  })
+  answer = answer.filter((v,i) => answer.indexOf(v) === i);
+  answer.sort((a,b) => a - b); 
+  return answer;
+}
