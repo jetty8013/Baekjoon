4c04e198bbeecef4bf9394346642b544c59253b9
윤지원 <jetty8013@gmail.com>
2022-12-05 17:43:40 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.49 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
+++ 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
@@ -12,16 +12,16 @@
 */
 
 function solution(board, moves) {
-    let stack = []
+    let stack = [] // 인형 쌓기
     let result = 0;
     
-    moves.forEach(value => {
-        for(let i = 0; i < board.length; i++){
+    moves.forEach(value => { //찝게 움직이기
+        for(let i = 0; i < board.length; i++){//크레인이 내려오면서 인형 집는 반복문
             if(board[i][value-1] !== 0){
-                if(stack[stack.length-1] === board[i][value-1]){
+                if(stack[stack.length-1] === board[i][value-1]){ //stack의 마지막 인형과 크레인으로 집은 인형이 같을떄
                     result += 2
-                    board[i][value-1] = 0
-                    stack.pop()
+                    board[i][value-1] = 0 // 집은 인형 board자리 0으로 바꿔줌
+                    stack.pop() //쌓인 인형 하나 제거
                     break
                 }
                 stack.push(board[i][value-1])
@@ -30,6 +30,5 @@ function solution(board, moves) {
             }
         }
     })
-
   return result;
 }
