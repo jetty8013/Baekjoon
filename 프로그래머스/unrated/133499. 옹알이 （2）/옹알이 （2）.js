68aad2b799d3e3258e86bfc861e1c3417f603f23
윤지원 <jetty8013@gmail.com>
2022-12-05 17:41:00 +0900

    [unrated] Title: 옹알이 (2), Time: 0.20 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.js
+++ 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.js
@@ -2,16 +2,15 @@
 *  "aya", "ye", "woo", "ma" 
 * 1. 발음할수있는거 외에 다른게 있는지 확인
 * 2. 연속된게 포함됐는지 확인
-* 
 */
 
 function solution(babbling) {
-    const regx1 = /(aya|ye|woo|ma)\1/
-    const regx2 = /^(aya|ye|woo|ma)+$/
+    const regx1 = /(aya|ye|woo|ma)\1/ //ayaaya , yeye , woowoo , mama 처럼 중복인지 찾는 조건
+    const regx2 = /^(aya|ye|woo|ma)+$/ // "aya", "ye", "woo", "ma" 이외에 다른 문자가 없는 조건
     let count = 0
     
     babbling.forEach(value => {
-        if(!regx1.exec(value) && regx2.exec(value)) count++
+        if(!regx1.test(value) && regx2.test(value)) count++ // (중복)이 아니면서 (다른 문자)가 없는 조건
     })
 
     return count
