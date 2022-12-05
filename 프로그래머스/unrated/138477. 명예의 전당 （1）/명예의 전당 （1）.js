be3cc720218746c6c18ae0f7c50850cc1c8884f0
윤지원 <jetty8013@gmail.com>
2022-12-05 17:20:30 +0900

    [unrated] Title: 명예의 전당 (1), Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.js
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.js
@@ -1,15 +1,21 @@
 /**
 * 매일매일 최상위에서 k개 만큼 뽑아서 그중에 최하위 반환
+* 
+* 1. score 요소 순회
+* 2. rank 배열에 score 요소 push
+* 3. rank 배열 내림차순으로 정렬
+* 4. k보다 rank 의 길이가 짧으면 rank 배열 중에서 가장 작은 요소 반환
+* 5. rank 배열 길이가 k-1 이상이면 rank[k-1] 반환 (명예의전당 k등)
 */
 function solution(k, score) {
   const answer = [];
-  const arr = [];
+  const rank = [];
 
   score.forEach(value => {
-    arr.push(value)
-    arr.sort((a,b) => b - a)
-    if(arr.length < k) answer.push(Math.min(...arr))
-    else answer.push(arr[k-1])
+    rank.push(value)
+    rank.sort((a,b) => b - a)
+    if(rank.length < k) answer.push(Math.min(...rank))
+    else answer.push(rank[k-1])
   });
 
   return answer;
