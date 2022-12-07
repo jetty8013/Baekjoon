2bb2402cb53eec8081cfd634e85a4a9e9a13e78e
윤지원 <jetty8013@gmail.com>
2022-12-07 16:21:59 +0900

    [unrated] Title: 기사단원의 무기, Time: 1.69 ms, Memory: 37.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.js
@@ -0,0 +1,30 @@
+/**
+* 1. 1~number 까지 약수구하기
+* 2. limit를 넘으면 power로 대체
+* 3. 다 더한값 출력
+*/
+
+function solution(number, limit, power) {
+    const numberArr = new Array(number).fill(0).map((el,index) => getDivisors(index+1))
+
+    const answer = numberArr.reduce((acc,cur) => {
+        if(cur > limit) return acc + power
+        return acc + cur
+    });
+    return answer;
+}
+
+
+function getDivisors(num){
+
+  const divisors = [];
+  for(let i = 1 ; i <= Math.sqrt(num) ; i++){
+      if(num % i === 0) {
+          divisors.push(i);
+          if(num / i != i) divisors.push(num / i);
+      }
+  }
+  
+  return divisors.length;
+
+}
