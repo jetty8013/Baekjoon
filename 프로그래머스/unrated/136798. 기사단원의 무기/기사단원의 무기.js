73a84fc99bf126eebdc79836ce3538d3729941e2
윤지원 <jetty8013@gmail.com>
2022-12-07 18:58:48 +0900

    [unrated] Title: 기사단원의 무기, Time: 1.27 ms, Memory: 35.9 MB -BaekjoonHub

--- 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.js
+++ 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.js
@@ -16,15 +16,12 @@ function solution(number, limit, power) {
 
 
 function getDivisors(num){
-
-  const divisors = [];
+  let divisors = 0;
   for(let i = 1 ; i <= Math.sqrt(num) ; i++){
       if(num % i === 0) {
-          divisors.push(i);
-          if(num / i != i) divisors.push(num / i);
+          divisors++;
+          if(num / i != i) divisors++;
       }
   }
-  
-  return divisors.length;
-
+  return divisors;
 }
