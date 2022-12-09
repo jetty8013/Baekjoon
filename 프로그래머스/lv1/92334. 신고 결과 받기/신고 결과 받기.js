b8a52e0927357e571b7aa4a00cb634e83ceed871
윤지원 <jetty8013@gmail.com>
2022-12-09 19:27:13 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.18 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.js
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.js
@@ -1,47 +1,44 @@
-/**
- * id_list
- * 중복횟수 객체 생성
- * report로 순회
- * 정지기준 k
- */
-
 function solution(id_list, report, k) {
-  const reportMessage = [] //메세지 누적
-  const reportUser = {} //
-  const reportedUser = {} //
-  const result = {}
+  const user = {
+    reportId:{},
+    reportedId:{},
+    blockId:{},
+    message : []
+  } //
+  
   id_list.forEach(el => {
-    reportedUser[el] = 0
-    result[el] = 0
+    user.reportedId[el] = 0
+    user.blockId[el] = 0
   })
 
   report.forEach(element => { //신고 기능
-    [report,reported] = element.split(" ")
-    reportMessage.push(report+"가 "+ reported + "를 신고했습니다.") // 메세지 누적
-    if(reportUser[`${report}`] === undefined) {
-      reportUser[`${report}`] = [reported]
-      reportedUser[reported] += 1
+    const [left,right] = element.split(" ") // 메세지 구조분해
+    //user.message.push(left+"가 "+ right + "를 신고했습니다.") // 메세지 누적
+
+    if(user.reportId[`${left}`] === undefined) {
+      user.reportId[`${left}`] = [right]
+      user.reportedId[right] += 1
     }
-    else if(reportUser[`${report}`].includes(reported) === false) {
-      reportUser[`${report}`].push(reported)
-      reportedUser[reported] += 1
+
+    else if(user.reportId[`${left}`].includes(right) === false) {
+      user.reportId[`${left}`].push(right)
+      user.reportedId[right] += 1
     }
   });
 
-  for(let i in reportUser){
+  for(let i in user.reportId){
     let num = 0
-    reportUser[i].forEach(el => {
-      if(reportedUser[el] >= k) num++
+    user.reportId[i].forEach(el => {
+      if(user.reportedId[el] >= k) num++
     })
-    result[i] = num
+    user.blockId[i] = num
   }
 
   //신고된 아이디 기준으로 정지된 ID 횟수 출력
-  // console.log(reportUser)
-  // console.log(reportedUser)
-  // console.log(result)
-
-  return Object.values(result)
+  //  console.log(user.reportId) 
+  //  console.log(user.reportedId) //{ muzi: 1, frodo: 2, apeach: 0, neo: 2 }
+  //  console.log(user.blockId) //{ muzi: 2, frodo: 1, apeach: 1, neo: 0 }
+  return Object.values(user.blockId)
 }
 
 /*
