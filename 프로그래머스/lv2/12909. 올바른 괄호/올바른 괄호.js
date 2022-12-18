36ab84033cb2ebd8f13803466239b1e2829bc2c8
윤지원 <jetty8013@gmail.com>
2022-12-18 20:36:43 +0900

    [level 2] Title: 올바른 괄호, Time: 2.42 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,8 +1,14 @@
+/**
+* '(' 문자로 열렸으면 반드시 짝지어서 ')' 문자로 닫혀야 한다
+* 1. ')'으로 시작하면 false
+*/
+
 function solution(s){
-    let stackCount = 0;
-    for (let i = 0; i < s.length; i++) {
-        stackCount += s[i] === '(' ? 1 : -1;
-        if (stackCount < 0) return false;
-    }
-    return stackCount === 0 ? true : false;
+    let count = 0;
+    for(let i=0;i<s.length;i++){
+        s[i] === '(' ? count+=1:count-=1
+        if(count < 0) return false
+    } 
+    if(count === 0) return true
+    else return false
 }
